7a1b0217372ea4ec8611ff66df6468f508d2bdd0
Richárd Szilágyi <54402153+mechwart@users.noreply.github.com>
2025-01-10 16:18:40 +0100

    implementation of home screen and some refactor

--- app/src/main/java/com/example/myimgflipapp/ui/navigation/Navigation.kt
+++ app/src/main/java/com/example/myimgflipapp/ui/navigation/Navigation.kt
@@ -5,15 +5,27 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.myimgflipapp.ui.screens.AboutScreen
+import com.example.myimgflipapp.ui.screens.HomeScreen
 import com.example.myimgflipapp.ui.screens.MemeScreen
 
 @Composable
 fun Navigation(modifier: Modifier) {
     val navController = rememberNavController()
 
-    NavHost(navController, startDestination = "memeScreen") {
+    NavHost(navController, startDestination = "homeScreen") {
         composable("memeScreen") {
             MemeScreen(modifier = modifier)
+        }
+        composable("aboutScreen") {
+            AboutScreen()
+        }
+        composable("homeScreen") {
+            HomeScreen(navigationToMakeMemes = {
+                navController.navigate("memeScreen")
+            }, navigationToAbout = {
+                navController.navigate("aboutScreen")
+            })
         }
     }
 }
